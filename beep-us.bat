bd197e91bad1f9f1044323ae565fb77e5b510e69
Denir Li <denir-li@users.noreply.github.com>
2015-12-18 19:22:21 +0800

    beep-us.bat
    beep-us.exe example: Mario - Victory Theme

---
+++ beep-us.bat
@@ -0,0 +1,40 @@
+:: Mario - Victory Theme
+:: Author: Adam Taylor <https://github.com/ataylor32>
+:: https://gist.github.com/ataylor32/24f429e147d8b2a758d7
+:: http://blog.dhampir.no/content/fun-with-beep
+
+@CLS
+@ECHO OFF
+@echo.
+@echo.
+
+beep-us.exe 130 100000
+beep-us.exe 262 100000
+beep-us.exe 330 100000
+beep-us.exe 392 100000
+beep-us.exe 523 100000
+beep-us.exe 660 100000
+beep-us.exe 784 300000
+beep-us.exe 660 300000
+
+beep-us.exe 146 100000
+beep-us.exe 262 100000
+beep-us.exe 311 100000
+beep-us.exe 415 100000
+beep-us.exe 523 100000
+beep-us.exe 622 100000
+beep-us.exe 831 300000
+beep-us.exe 622 300000
+
+beep-us.exe 155 100000
+beep-us.exe 294 100000
+beep-us.exe 349 100000
+beep-us.exe 466 100000
+beep-us.exe 588 100000
+beep-us.exe 699 100000
+beep-us.exe 933 300000
+
+beep-us.exe 933 100000
+beep-us.exe 933 100000
+beep-us.exe 933 100000
+beep-us.exe 1047 400000
